c05caf7eb3d2aa45b9e55eec5c3aca99c21e128c
IDStack <idstack@outlook.co.id>
2022-02-05 01:21:41 +0700

    prepare schemas for backend use sanity

--- backend/schemas/schema.js
+++ backend/schemas/schema.js
@@ -4,6 +4,13 @@ import createSchema from 'part:@sanity/base/schema-creator'
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
+import user from './user'
+import card from './card'
+import comment from './comment'
+import postedBy from './postedBy'
+import save from './save'
+
+
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
@@ -11,6 +18,6 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    /* Your types here! */
+    user, card, comment, postedBy, save
   ]),
 })
